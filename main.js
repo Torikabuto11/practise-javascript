7bbfc24a10b263dc5bf6e5c11912a1fc2763c41c
Torikabuto11 <toarukabu@gmail.com>
2024-09-19 05:19:03 +0000

    9/19JavaScriptの導入

---
+++ main.js
@@ -0,0 +1,17 @@
+document.querySelector("h1").textContent = "自分の名前"
+document.querySelector("h2").textContent = "masudaK"
+
+//documentとは！！
+/*htmlファイルで書かれているコードを一つのオブジェクトとしてみなします。*/
+
+//DOM(ドム)
+/*ドキュメントオブジェクトモデル(Document Object Model)の略で、Webページの要素やコンテンツなどをツリー構造で表現したデータモデル*/
+
+//上記コードの意味
+//docyument.querySelector("h1").textContent="自分の名前"
+
+//ドキュメントの中の("h1")タグを探してね
+//docyument.querySelector("h1")
+
+//h1タグの中身のtextを変えてね
+//.textContent="自分の名前"
