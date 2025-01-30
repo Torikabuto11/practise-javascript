c069fb1228cfa425d84aa0282e69afd34ae2dfb6
Torikabuto11 <toarukabu@gmail.com>
2025-01-30 05:13:46 +0000

    カウントの追加

--- main.js
+++ main.js
@@ -72,9 +72,16 @@ function NumberGuessGame(){
         }else{
             message.textContent = "小さい"
         }
+        count++;
+        countDisplay.textContent = `試行回数: ${count}`;
+
     })
 
     gameContainer.appendChild(button);
+    let count= 0;
+    const countDisplay = document.createElement("p")
+    countDisplay.textContent = `試行回数: ${count}`;
+    gameContainer.appendChild(countDisplay)
 }
 
 NumberGuessGame();
